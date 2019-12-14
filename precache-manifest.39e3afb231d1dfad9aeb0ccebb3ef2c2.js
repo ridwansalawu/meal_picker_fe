0c547941f3ad0771d975d9c7e8620f5cc2b83278
ridwansalawu <reedysalawu@gmail.com>
2019-12-14 17:33:34 +1100

    Updates

---
+++ precache-manifest.39e3afb231d1dfad9aeb0ccebb3ef2c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9398d05705a46e38ed9d6a0621e068",
+    "url": "/meal_picker_fe/index.html"
+  },
+  {
+    "revision": "0349c2e8c9cb4521fff1",
+    "url": "/meal_picker_fe/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "c142c70de9bbd14baa4c",
+    "url": "/meal_picker_fe/static/css/main.fcd88f41.chunk.css"
+  },
+  {
+    "revision": "0349c2e8c9cb4521fff1",
+    "url": "/meal_picker_fe/static/js/2.c38bfbac.chunk.js"
+  },
+  {
+    "revision": "dda0b0bcdad2e5ff43f0aab855f6f485",
+    "url": "/meal_picker_fe/static/js/2.c38bfbac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c142c70de9bbd14baa4c",
+    "url": "/meal_picker_fe/static/js/main.b0852da3.chunk.js"
+  },
+  {
+    "revision": "abc16daca82b9812aacd",
+    "url": "/meal_picker_fe/static/js/runtime-main.5c780ebd.js"
+  }
+]);
